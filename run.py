4aa9635063542fe67ab47f503f8d234c6e86fd39
supertoxicalien <supertoxicalien@gmail.com>
2022-05-23 17:48:54 +0300

    template refactor

--- run.py
+++ run.py
@@ -5,7 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', titile='Main Page')
+    return render_template('index_2.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about_2.html')
 
 
 if __name__ == '__main__':
